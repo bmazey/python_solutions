e924675273429b91277eca5e3becb924e9c1ef67
Brandon <b.mazey@nyu.edu>
2018-06-14 13:14:56 -0400

    good introspection example up

--- exercises/structures/src/movie_queue.py
+++ exercises/structures/src/movie_queue.py
@@ -1,8 +1,10 @@
 class MovieQueue:
+    """MovieQueue class."""
     def __init__(self):
         self.queue = list()
 
     def populate_movie_queue(self):
+        """populates a new MovieQueue"""
         self.queue.insert(0, 'Donatello')
         self.queue.insert(1, 'Raphael')
         self.queue.insert(2, 'Michelangelo')
