323bf58628f88494a74bda730aacc0af5f0ccb9b
Brandon <b.mazey@nyu.edu>
2018-06-17 16:25:47 -0400

    base maze done ...

---
+++ exercises/patterns/factory/maze.py
@@ -0,0 +1,17 @@
+# borrowed from http://pythoninthepink.blogspot.com/
+
+class Maze(object):
+
+    """Maze contains Rooms"""
+
+    def __init__(self):
+        self.rooms = {}
+
+    def add_room(self, room):
+        """Add a Room"""
+        self.rooms[room.room_number] = room
+
+    def get_room_by_number(self, room_number):
+        """Get Room by its number"""
+        return self.rooms[room_number]
+
