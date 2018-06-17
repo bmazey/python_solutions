362541b68643bc89cfe15f0bffbdd9551421e366
Brandon <b.mazey@nyu.edu>
2018-06-17 16:59:51 -0400

    adding player support ...

--- exercises/patterns/factory/map_site.py
+++ exercises/patterns/factory/map_site.py
@@ -1,6 +1,10 @@
 # borrowed from http://pythoninthepink.blogspot.com/
 
 class MapSite(object):
+
     """Abstract for Maze elements"""
-    pass
+
+    def enter(self, player):
+        """Entering this abstract site can't be good..."""
+        print(player.player_id, "says: Ouch!")
 
