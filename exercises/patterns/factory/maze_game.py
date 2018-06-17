cb23227527fa051a134c593d65342512603481b9
Brandon <b.mazey@nyu.edu>
2018-06-17 18:42:38 -0400

    looking great! got tripped up a bit - python is TOO FORGIVING!

--- exercises/patterns/factory/maze_game.py
+++ exercises/patterns/factory/maze_game.py
@@ -6,8 +6,11 @@ from exercises.patterns.factory.room import Room
 from exercises.patterns.factory.door import Door
 from exercises.patterns.factory.wall import Wall
 from exercises.patterns.factory.player import Player
+from exercises.patterns.factory.wizard import Wizard
 from exercises.patterns.factory.enchanted_room import EnchantedRoom
 from exercises.patterns.factory.enchanted_door import EnchantedDoor
+from exercises.patterns.factory.maze_factory import MazeFactory
+from exercises.patterns.factory.enchanted_maze_factory import EnchantedMazeFactory
 
 def create_maze():
     """Series of operations which create our Maze."""
@@ -53,6 +56,30 @@ def create_enchanted_maze():
 
     return maze
 
+# this is the one we care the most about!
+def create_factory_maze(factory):
+    """Series of operations which create our Maze."""
+
+    maze = factory.make_maze()
+    room1 = factory.make_room(1)
+    room2 = factory.make_room(2)
+    door = factory.make_door(room1, room2)
+
+    maze.add_room(room1)
+    maze.add_room(room2)
+
+    room1.set_side(Direction.NORTH, factory.make_wall())
+    room1.set_side(Direction.EAST, door)
+    room1.set_side(Direction.SOUTH, factory.make_wall())
+    room1.set_side(Direction.WEST, factory.make_wall())
+
+    room2.set_side(Direction.NORTH, factory.make_wall())
+    room2.set_side(Direction.EAST, factory.make_wall())
+    room2.set_side(Direction.SOUTH, factory.make_wall())
+    room2.set_side(Direction.WEST, door)
+
+    return maze
+
 def play(maze, player):
     print("\n--> Player {} enters the maze in room {}".format(player.player_id, player.current_room.room_number))
     room1 = player.current_room
@@ -77,14 +104,15 @@ def play_enchanted(maze, player):
 
     door = room1.get_side(Direction.EAST)
     player.unlock_with_spell(door)
-    player = door.enter(player)
+    door.enter(player)
 
+    room2 = player.current_room
     for side in Direction.ALL:
-        print("\t{} SIDE: {}".format(side, room1.get_side(side)))
+        print("\t{} SIDE: {}".format(side, room2.get_side(side)))
 
 if __name__ == "__main__":
-    maze = create_maze()
-    enchanted_maze = create_enchanted_maze()
+    maze = create_factory_maze(MazeFactory)
+    enchanted_maze = create_factory_maze(EnchantedMazeFactory)
     play(maze, Player("Pinky", maze.get_room_by_number(1)))
-    play_enchanted("Harry", enchanted_maze.get_room_by_number(1))
+    play_enchanted(enchanted_maze, Wizard("Harry", enchanted_maze.get_room_by_number(1)))
 
