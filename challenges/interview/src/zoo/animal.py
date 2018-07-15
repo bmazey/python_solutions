eb5cbc0397dece9cf302440ef0521a5d7b092d29
Brandon <b.mazey@nyu.edu>
2018-07-15 16:16:10 -0400

    using double-quotes for animal string

---
+++ challenges/interview/src/zoo/animal.py
@@ -0,0 +1,10 @@
+class Animal:
+    """this is our animal interface"""
+
+    # all Animals have a name
+    def __init__(self, name):
+        self.name = name
+
+    # all Animals eat
+    def eat(self):
+        return "munch ... munch ..."
