fcfbdfc466c5ba4cd6294f8ffbc3f9262691643b
Brandon <b.mazey@nyu.edu>
2018-07-16 12:41:10 -0400

    verify working

--- exercises/fundamentals/src/verify.py
+++ exercises/fundamentals/src/verify.py
@@ -1,4 +1,4 @@
 
 
-# TODO - change PyCharm theme!
-print("congratulations! don't forget to email your github username to b.mazey@nyu.edu!")
+def verify_install():
+    return "congratulations! don't forget to email your github username to b.mazey@nyu.edu!"
