3d192a9919f1abba2434765f952b12d9c0bed27f
Brandon <b.mazey@nyu.edu>
2018-07-16 18:37:50 -0400

    added lists and tuples

---
+++ exercises/fundamentals/src/tuples.py
@@ -0,0 +1,32 @@
+
+
+def main():
+    # tuples are immutable
+    tuple1 = (1, 2, 3)
+
+    # lists are mutable
+    list1 = [1, 2, 3]
+
+    tuple2 = append_to_sequence(tuple1)
+    list2 = append_to_sequence(list1)
+
+    # outputs (1, 2, 3)
+    print('tuple1 = ', tuple1)
+
+    # outputs (1, 2, 3, 9, 9, 9)
+    print('tuple2 = ', tuple2)
+
+    # outputs [1, 2, 3, 9, 9, 9]
+    print('list1 = ', list1)
+
+    # outputs [1, 2, 3, 9, 9, 9]
+    print('list2 = ', list2)
+
+
+def append_to_sequence(sequence):
+    sequence += (9, 9, 9)
+    return sequence
+
+
+if __name__ == '__main__':
+    main()
