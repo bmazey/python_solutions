dda7ed7b0ac2c6e15bb4a27cbadf749b5cb79057
Brandon <b.mazey@nyu.edu>
2018-07-29 22:50:27 -0400

    path is broken - needs fix!

--- challenges/rumors/src/__init__.py
+++ challenges/rumors/src/__init__.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 db.create_all()
--- challenges/rumors/src/application.py
+++ challenges/rumors/src/application.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restplus import Resource, Api
+from challenges.rumors.src.rumor import Rumor
+from challenges.rumors.src import db
 
 
 application = Flask(__name__)
@@ -10,6 +12,11 @@ api = Api(application)
 class RumorRoute(Resource):
     def get(self):
         return {'brandon': 'listens to selena gomez'}
+
+    def post(self):
+        rumor = Rumor(name='brandon', content='has a jonas brothers poster')
+        db.session.add(rumor)
+        db.session.commit()
 
 
 def get_app():
