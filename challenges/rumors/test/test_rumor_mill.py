bf85d27af0ed37dee9d3def80059ac773ae026c3
Brandon <b.mazey@nyu.edu>
2018-07-31 10:30:37 -0400

    flaskr not working on 3.7?

--- challenges/rumors/test/test_rumor_mill.py
+++ challenges/rumors/test/test_rumor_mill.py
@@ -9,10 +9,16 @@ class RumorMillTest(TestCase):
         self.application = get_app()
         self.client = self.application.test_client()
 
-    def test_index(self):
+    def test_simple_get(self):
         response = self.client.get('/rumor')
         print(response.get_json())
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('brandon') == 'listens to selena gomez')
+
+    def test_post_and_get(self):
+        response = self.client.post('/rumor', data=dict(name='alyssa', content='cheated on her spanish homework'))
+        print(response.get_json())
+        self.assertTrue(200, response.status_code)
+        # self.assertTrue(response.get_json().get('brandon') == 'listens to selena gomez')
 
 
